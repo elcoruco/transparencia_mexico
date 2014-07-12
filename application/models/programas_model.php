5a6de0c02e025da5f16507dc1f7f64844d1c1324
el coruco <el.coruco@gmail.com>
2014-07-12 13:34:58 -0500

    ni sé qué tiene esto

---
+++ application/models/programas_model.php
@@ -0,0 +1,20 @@
+<?php if(! defined('BASEPATH')) exit('HOWDY amigo!');
+
+/**
+* Programas_model
+*
+* @author elcoruco
+*/
+class Programas_model extends CI_Model{
+  const TABLE = "pp_2010_2014";
+  
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function count_by_ramo($ramo){
+
+  }
+
+}
